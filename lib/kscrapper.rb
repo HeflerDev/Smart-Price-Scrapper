5eb3348e0df303efc7542aa8d5c60c25de647109
Kasuhira <hheflerdevelopment@gmail.com>
2020-02-02 18:03:55 -0300

    Fixed Linter Issues

--- lib/kscrapper.rb
+++ lib/kscrapper.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
-class KScrapper 
+# rubocop:disable Metrics/MethodLength,Metrics/LineLength,Style/ClassVars
 
+class KScrapper
   attr_accessor :prices_databank
 
   @@prices_databank = {}
@@ -19,17 +20,18 @@ class KScrapper
     @name_data = []
     @value_data = []
   end
-#Function that Collects and return data in form of array.
+
+  # Function that Collects and return data in form of array.
   def collect_data
     @@search_container.push(@search_term)
     puts "Search has Returned #{@parsed_content.css('.s-item__wrapper').length} results. Press enter to proceed."
     gets.chomp
     @parsed_content.css('.s-item__wrapper').each do |row|
-      title   = row.css('.s-item__title').inner_text
-      status  = row.css('.SECONDARY_INFO').inner_text
-      price   = row.css('.s-item__price').inner_text
-      logistic= row.css('.s-item__logisticsCost').inner_text
-      sold    = row.css('.NEGATIVE').inner_text
+      title = row.css('.s-item__title').inner_text
+      status = row.css('.SECONDARY_INFO').inner_text
+      price = row.css('.s-item__price').inner_text
+      logistic = row.css('.s-item__logisticsCost').inner_text
+      sold = row.css('.NEGATIVE').inner_text
       puts '|=============================================================================|'
       puts title
       puts status
@@ -37,36 +39,34 @@ class KScrapper
       puts logistic
       puts sold
       @name_data.push(title)
-      @brute_collect.push(price)  
+      @brute_collect.push(price)
     end
-    @clean_values = @brute_collect.map do   |x|
-      #This line Filters only numbers and punctuation, transform the numbers into floats and if the array size is bigger than 2, takes the average value
-      filter_data = ->(x) { x.scan(/\d+[,.]\d+/).to_s.split('').map {|x| x == ',' ? '.' : x}.join('').scan(/\d+[,.]\d+/).map(&:to_f).inject{|x,y| (x+y)/2}}
+    @clean_values = @brute_collect.map do |x|
+      # This line Filters only numbers and punctuation, transform the numbers into floats and if the array size is bigger than 2, takes the average value
+      filter_data = ->(a) { a.scan(/\d+[,.]\d+/).to_s.split('').map { |u| u == ',' ? '.' : u }.join('').scan(/\d+[,.]\d+/).map(&:to_f).inject { |k, z| (k + z) / 2 } }
       @value_data.push(filter_data.call(x))
       filter_data.call(x)
     end
-    #This line Stores the Data on a Databank
+    # This line Stores the Data on a Databank
     @@prices_databank[@search_term] = @clean_values
     @@name_storage[@name_data] = @value_data
-  end  
-  
-  def KScrapper.show_databank(answer)
+  end
 
-    
-    if answer == 0
+  def self.show_databank(answer)
+    if answer.zero?
       @@prices_databank.each do |x, y|
         puts '----------------------------------------------------------------------'
         puts " | #{x.capitalize} : #{y} | "
       end
     else
-      @@name_storage.each_with_index do |(k, v), i| 
+      @@name_storage.each_with_index do |(k, v), i|
         puts '============================================================='
         puts '                                                             '
         puts "                      #{@@search_container[i]}               "
         puts '                                                             '
         puts '============================================================='
         puts
-        k.each_with_index do |x,y| 
+        k.each_with_index do |_, y|
           puts "#{k[y]} : #{v[y]}"
           puts '-----------------------------------------------------------------'
         end
@@ -74,28 +74,28 @@ class KScrapper
     end
   end
 
-  def KScrapper.compute_average
+  def self.compute_average
     @@prices_databank.each do |key, value|
-      @@storage[key] = value.inject{ |x, y| x+y}/value.length
+      @@storage[key] = value.inject { |x, y| x + y } / value.length
       puts '----------------------------------------------------'
       puts "#{key.capitalize} : #{@@storage[key]}"
     end
   end
 
-  def KScrapper.compute_biggest
-    @@name_storage.each_with_index do |(x, y),z|
+  def self.compute_biggest
+    @@name_storage.each_with_index do |(x, y), z|
       index = y.index(y.max)
-      puts "====================================================| #{@@search_container[z]} |===================================================="
+      puts "=================================| #{@@search_container[z]} |==============================="
       puts "The biggest value is : #{x[index].capitalize} that costs : #{y[index]}"
     end
   end
 
-  def KScrapper.compute_smallest
-    @@name_storage.each_with_index do |(x, y),z|
+  def self.compute_smallest
+    @@name_storage.each_with_index do |(x, y), z|
       index = y.index(y.min)
-      puts "====================================================| #{@@search_container[z]} |===================================================="
+      puts "=================================| #{@@search_container[z]} |==============================="
       puts "The smallest value is : #{x[index].capitalize} that costs : #{y[index]}"
     end
   end
 end
-
+## rubocop:enable Metrics/MethodLength,Metrics/LineLength,Style/ClassVars
