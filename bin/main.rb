23d64f68df9ca305a7077017869ec4113460a1e9
Kasuhira <hheflerdevelopment@gmail.com>
2020-02-05 16:36:23 -0300

    Changed "str_content" file location to bin

--- bin/main.rb
+++ bin/main.rb
@@ -7,6 +7,7 @@ require 'rest-client'
 require 'open-uri'
 require 'tty-prompt'
 
+require_relative 'str_content.rb'
 require_relative '../lib/kscrapper.rb'
 
-object  = KScrapper.new("https://www.ebay.com/sch/i.html?_from=R40&_nkw=coffee+cup", 'coffee+cup')
+
---
+++ bin/str_content.rb
@@ -0,0 +1,18 @@
+def greetings
+  Gem.win_platform? ? (system 'cls') : (system 'clear')
+  puts
+  puts 'Welcome to Kasuhira Price Scrapping system, how may I be of assistance ?'
+  puts
+  puts '     _________'
+  puts '    / ======= \\'
+  puts '   / __________\\'
+  puts '  | ___________ |'
+  puts '  | | -       | |'
+  puts '  | |         | |'
+  puts '  | |_________| |_____________________________'
+  puts '  \=____________/   Developed by Kasuhira     )'
+  puts '  / """"""""""" \                            /'
+  puts " / ::::::::::::: \\                      =D -'"
+  puts '|_________________|'
+puts
+end
